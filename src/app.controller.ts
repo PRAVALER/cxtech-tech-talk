e1f138b1ad1a3cb4b7a8dbcd73e73da187d260fd
Bruno Lima <bruno.lima@pravaler.com.br>
2022-01-24 17:18:31 -0300

    Criar função de potência
    Criar função para exemplificar uso de uma função em um PR.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PowService } from './pow/pow.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly powService: PowService,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return `Hello ${this.powService.pow2(5)}`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PowService } from './pow/pow.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PowService],
 })
 export class AppModule {}
